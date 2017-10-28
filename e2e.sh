32a252a2f6ddab0cd77d4a6bc3b612756b28b282
Ivan Smirnov <isgsmirnov@gmail.com>
2017-10-27 19:06:25 -0700

    fix minor bug in e2e test

--- e2e.sh
+++ e2e.sh
@@ -8,6 +8,7 @@ fi
 
 # start zap, fork.
 ./zap --port 16000 1>/dev/null 2>/dev/null &
+ZAP_PID=$!
 
 # pull results.
 RESP="$(curl -I -L -H 'Host: g' localhost:16000/z 2>/dev/null | head -n 2)"
@@ -15,13 +16,18 @@ RESP="$(curl -I -L -H 'Host: g' localhost:16000/z 2>/dev/null | head -n 2)"
 # Check header
 if [[ $RESP != *"HTTP/1.1 302 Found"* ]]; then
     echo "302 status not found"
+    echo "got: $RESP"
+    kill -9 $ZAP_PID
     exit 2
 fi
 
 # check location
 if [[ $RESP != *"Location: https://github.com/issmirnov/zap"* ]]; then
     echo "Location is wrong"
+    echo "Got: $RESP"
+    kill -9 $ZAP_PID
     exit 3
 fi
 
+kill -9 $ZAP_PID
 echo "End to end test passed."
